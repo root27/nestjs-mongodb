0cb8edb9c7b2302b30101a338cc7088aa997fe01
root27 <oguzhaneee@gmail.com>
2024-07-09 17:28:57 +0300

    remove unnecessary mongooseModel import and add AppController for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './schemas/user.schema';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest', { dbName: 'nest' }),
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     UsersModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
